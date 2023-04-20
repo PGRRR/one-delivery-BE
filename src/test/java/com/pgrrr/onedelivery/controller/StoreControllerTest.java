7c9975789317710bf33f3724356cf23bc5dcdfae
pgrrr <pgrrr119@gmail.com>
2023-04-20 20:27:44 +0900

    test: 매장 Controller 테스트 코드 작성
    - 매장 리스트 카테고리, 카테고리 + 주소 조회 테스트 코드 작성

---
+++ src/test/java/com/pgrrr/onedelivery/controller/StoreControllerTest.java
@@ -0,0 +1,119 @@
+package com.pgrrr.onedelivery.controller;
+
+import com.google.gson.Gson;
+import com.pgrrr.onedelivery.dto.StoreResponseDto;
+import com.pgrrr.onedelivery.service.StoreService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class StoreControllerTest {
+
+    @InjectMocks
+    private StoreController storeController;
+
+    @Mock
+    private StoreService storeService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(storeController).build();
+    }
+
+    @DisplayName("매장 리스트 카테고리 아이디로 API 조회 성공")
+    @Test
+    void loadStoreListByCategoryIdSuccess() throws Exception {
+        //given
+        doReturn(storeListByCategoryId()).when(storeService).getStoreListByCategory(1L);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get("/stores?categoryId=1")
+        );
+
+        //then
+        MvcResult mvcResult = resultActions.andExpect(status().isOk()).andReturn();
+
+        List<StoreResponseDto> storeResponseDtoList = new Gson().fromJson(mvcResult.getResponse().getContentAsString(), List.class);
+        assertEquals(storeResponseDtoList.size(), 10);
+    }
+
+    private List<StoreResponseDto> storeListByCategoryId() {
+        List<StoreResponseDto> storeResponseDtoList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            storeResponseDtoList.add(
+                    StoreResponseDto.builder()
+                            .storeId((long) i)
+                            .name("name" + i)
+                            .categoryId(1L)
+                            .build()
+            );
+        }
+        return storeResponseDtoList;
+    }
+
+    @DisplayName("매장 리스트 카테고리 아이디와 주소로 API 조회 성공")
+    @Test
+    void loadStoreListByCategoryIdAddressSuccess() throws Exception {
+        //given
+        doReturn(storeListByCategoryIdAddress()).when(storeService).getStoreListByCategory(1L);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get("/stores?categoryId=1&address=address")
+        );
+
+        //then
+        MvcResult mvcResult = resultActions.andExpect(status().isOk()).andReturn();
+
+        List<StoreResponseDto> storeResponseDtoList = new Gson().fromJson(mvcResult.getResponse().getContentAsString(), List.class);
+        assertEquals(storeResponseDtoList.size(), 1);
+    }
+
+    private List<StoreResponseDto> storeListByCategoryIdAddress() {
+        List<StoreResponseDto> storeResponseDtoList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            storeResponseDtoList.add(
+                    StoreResponseDto.builder()
+                            .storeId((long) i)
+                            .name("name" + i)
+                            .address("address")
+                            .categoryId((long) i)
+                            .build()
+            );
+        }
+        return storeResponseDtoList;
+    }
+
+    @Test
+    void loadStoreListByCategory() {
+    }
+
+    @Test
+    void loadStoreListByCategoryAddress() {
+    }
+
+    @Test
+    void getStore() {
+    }
+}
---
+++ src/test/java/com/pgrrr/onedelivery/service/StoreServiceTest.java
@@ -0,0 +1,79 @@
+package com.pgrrr.onedelivery.service;
+
+import com.pgrrr.onedelivery.domain.Store;
+import com.pgrrr.onedelivery.dto.StoreResponseDto;
+import com.pgrrr.onedelivery.mapper.StoreMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import javax.swing.*;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class StoreServiceTest {
+
+    // Test 주체
+    @InjectMocks
+    StoreService storeService;
+
+    // Test 협력자
+    @Mock
+    StoreMapper storeMapper;
+
+
+    // Test를 실행하기 전마다 가짜 객체 주입
+    @BeforeEach
+    void setUp() {
+        storeService = new StoreService(storeMapper);
+    }
+
+    @Test
+    @DisplayName("카테고리 아이디로 음식점 조회 성공")
+    void getStoreListByCategory() {
+//        when(storeMapper.selectStoreListByCategory(anyLong())).thenReturn(anyList());
+//
+//        storeService.getStoreListByCategory(1L);
+//
+//        verify(storeMapper).selectStoreListByCategory(anyLong());
+        //given
+        StoreResponseDto storeResponseDto = StoreResponseDto.builder()
+                .storeId(1L)
+                .name("name")
+                .phone("phone")
+                .categoryId(1L)
+                .build();
+
+
+        when(storeMapper.selectStoreListByCategory(anyLong())).thenReturn(anyList());
+
+        //when
+        List<StoreResponseDto> storeListByCategory = storeService.getStoreListByCategory(1L);
+
+        //then
+
+    }
+
+    @Test
+    void getStoreListByCategoryAddress() {
+    }
+
+    @Test
+    void getStore() {
+    }
+}
