4f891f8cb6f9ab1bdae4bcc4da4dea212c8cc327
pgrrr <pgrrr119@gmail.com>
2023-05-09 17:21:26 +0900

    feat: 매장 생성 요청 DTO 수정
    - 매장 생성 요청 DTO 생성자 추가
    - 롬복 사용
    - 추후 @JsonCreator 사용 예정

--- src/main/java/com/pgrrr/onedelivery/dto/StoreRequestDto.java
+++ src/main/java/com/pgrrr/onedelivery/dto/StoreRequestDto.java
@@ -1,14 +1,15 @@
 package com.pgrrr.onedelivery.dto;
 
 import com.pgrrr.onedelivery.domain.Store;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StoreRequestDto {
 
     @NotBlank(message = "매장 이름은 필수 입력 값입니다.")
@@ -37,6 +38,27 @@ public class StoreRequestDto {
 
     @NotNull(message = "매장 카테고리는 필수 입력 값입니다.")
     private Long categoryId;
+
+//    @JsonCreator
+//    public StoreRequestDto(@JsonProperty(value = "name") String name,
+//                           @JsonProperty(value = "phone") String phone,
+//                           @JsonProperty(value = "address") String address,
+//                           @JsonProperty(value = "info") String info,
+//                           @JsonProperty(value = "status") Store.StoreStatus status,
+//                           @JsonProperty(value = "img") String img,
+//                           @JsonProperty(value = "minCost") Long minCost,
+//                           @JsonProperty(value = "tipPrice") Long tipPrice,
+//                           @JsonProperty(value = "categoryId") Long categoryId) {
+//        this.name = name;
+//        this.phone = phone;
+//        this.address = address;
+//        this.info = info;
+//        this.status = status;
+//        this.img = img;
+//        this.minCost = minCost;
+//        this.tipPrice = tipPrice;
+//        this.categoryId = categoryId;
+//    }
 
     public Store toEntity() {
         return Store.builder()
